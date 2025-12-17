2d66908452c3e82b84176898110ab03a6cb1e5b3
Lin-Yuxia2025 <alfonzo55555@gmail.com>
2025-12-17 10:52:17 +0900

    vercel用にrun-python.pyを追加

--- api/index.py
+++ api/index.py
@@ -13,14 +13,16 @@ sys.path.append(str(py_scripts))
 import parse
 parse_run = parse.parse
 
+# Flaskが(このファイルで)起動していなければ(vercelで実行時)、/api/run-pythonで呼んでも、こっちは動かない
 @app.route('/api/run-python', methods=['POST'])
 def run_python():
-    state = request.get_json()
+    state = request.get_json()      # リクエストのJSON文字列をPythonの辞書に変換して取得
 
-    result = parse_run(state)
+    result = parse_run(state)       # 呼び出し実行
 
-    return jsonify(result)
+    return jsonify(result)          # Pythonの辞書をJSON形式のレスポンスに変換して返す
 
+# 直接実行(local)時のみ立ち上げて、import時は起動しない
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5328, debug=False, use_reloader=False)
+    app.run(host="127.0.0.1", port=5328)
 
